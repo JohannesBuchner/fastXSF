249bb54bc3b1a293c9f7985c61c8ea59a010f9b1
Johannes Buchner <johannes.buchner.acad@gmx.com>
2025-03-04 17:26:10 +0100

    add astropy as requirement

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('HISTORY.rst', encoding="utf-8") as history_file:
     history = re.sub(r':py:class:`([^`]+)`', r'\1', 
         history_file.read())
 
-requirements = ['numpy', 'scipy', 'matplotlib', 'corner', 'optns', 'tqdm', 'ultranest']
+requirements = ['numpy', 'scipy', 'matplotlib', 'corner', 'optns', 'tqdm', 'ultranest', 'astropy']
 
 setup_requirements = ['pytest-runner', ]
 
