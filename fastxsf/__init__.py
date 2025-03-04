79e1816156adc0d06614e9dc17618aa2c023ab90
Johannes Buchner <johannes.buchner.acad@gmx.com>
2025-03-04 15:24:23 +0100

    add packaging files

--- fastxsf/__init__.py
+++ fastxsf/__init__.py
@@ -1,3 +1,9 @@
+"""Fast X-ray spectral fitting."""
+
+__author__ = """Johannes Buchner"""
+__email__ = 'johannes.buchner.acad@gmx.com'
+__version__ = '1.2.0'
+
 from .data import load_pha
 from .model import x, Table, xvec, logPoissonPDF, logPoissonPDF_vectorized
 
