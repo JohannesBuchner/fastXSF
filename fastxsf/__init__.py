e4e6af42182deda8a97459bacb20a7cb031cdaaf
Johannes Buchner <johannes.buchner.acad@gmx.com>
2025-04-30 18:18:19 +0200

    Bump version: 1.0.0 → 1.1.0

--- fastxsf/__init__.py
+++ fastxsf/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Johannes Buchner"""
 __email__ = 'johannes.buchner.acad@gmx.com'
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 from .data import load_pha
 from .model import (FixedTable, Table, logPoissonPDF, logPoissonPDF_vectorized,
